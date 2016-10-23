de87e6d2b4b91ebea661e95ba09ee9e319f3871c
s.abdolmaleki <abdolmaleki.s@gmail.com>
2016-10-23 13:20:41 +0330

    AooSamim Uncommented

--- myapplication/src/main/java/com/example/myapplication/application/AppSamim.java
+++ myapplication/src/main/java/com/example/myapplication/application/AppSamim.java
@@ -9,11 +9,28 @@ import android.util.Log;
 
 import com.activeandroid.ActiveAndroid;
 import com.activeandroid.Configuration;
+import com.activeandroid.serializer.BigDecimalSerializer;
+import com.activeandroid.serializer.CalendarSerializer;
+import com.activeandroid.serializer.FileSerializer;
+import com.activeandroid.serializer.SqlDateSerializer;
+import com.activeandroid.serializer.TypeSerializer;
+import com.activeandroid.serializer.UUIDSerializer;
+import com.activeandroid.serializer.UtilDateSerializer;
 import com.example.myapplication.database.Db;
 import com.example.myapplication.database.model.ChatModel;
 import com.example.myapplication.database.model.StudentModel;
 import com.example.myapplication.database.model.TeacherModel;
+import com.example.myapplication.database.model.serializer.ChatContentDateSerializer;
+import com.example.myapplication.database.model.serializer.ChatContentTypeDateSerializer;
+import com.example.myapplication.entity.ChatBaseContent;
+import com.example.myapplication.entity.ChatBaseContentDeserializer;
+import com.example.myapplication.entity.ChatContentType;
+import com.example.myapplication.entity.ChatContentTypeDeserializer;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.GsonBuilder;
 
+import java.lang.reflect.Modifier;
+import java.util.Date;
 import java.util.UUID;
 
 import ir.hfj.library.application.App;
@@ -21,36 +38,49 @@ import ir.hfj.library.application.AppConfig;
 import ir.hfj.library.database.DbBase;
 import ir.hfj.library.database.model.UserSettingModel;
 import ir.hfj.library.receiver.SamimAction;
+import ir.hfj.library.util.ExclusionGsonStrategies;
+import microsoft.aspnet.signalr.client.DateSerializer;
 
-public class AppSamim extends App {
-    public AppSamim() {
+public class AppSamim extends App
+{
+
+    public AppSamim()
+    {
         //=====================================================================================================
         AppConfig.LOG_TAG = "Samim";
         AppConfig.DEBUG = true;
         AppConfig.ROLE = 1;
+
+
 //        AppConfig.NETWORK_HOST_WS = "http://samim.aligorji.ir:8080/api";
-        AppConfig.NETWORK_HOST_SR = "http://samim.aligorji.ir:8080/";
+//        AppConfig.NETWORK_HOST_SR = "http://samim.aligorji.ir:8080/";
 //        AppConfig.NETWORK_HOST_WEB = "http://samim.aligorji.ir:8080/";
-//        AppConfig.NETWORK_Samim_HUB = "SamimParentHub";
-//        AppConfig.NETWORK_HOST_TRY_CONNECT_DURATION = 1000 * 3;
-//        AppConfig.NETWORK_HOST_PING = AppConfig.NETWORK_HOST_SR + "TestConnection/Ping.html";
-//        AppConfig.NETWORK_HOST_TRY_CONNECT_EMERGENCY_DURATION = 1000 * 10;//1000 * 60 * 30;
-//
-//        AppConfig.AUTO_REFRESH_INTERVAL = 30;//minute
-//
-//        AppConfig.setGsonSetting(new GsonBuilder()
-//                                         .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-//                                         .excludeFieldsWithModifiers(Modifier.TRANSIENT)
-//                                         .registerTypeAdapter(Date.class, new DateSerializer())
-//                                         .registerTypeAdapter(ChatBaseContent.class, new ChatBaseContentDeserializer())
-//                                         .registerTypeAdapter(ChatContentType.class, new ChatContentTypeDeserializer())
-//                                         .setExclusionStrategies(new ExclusionGsonStrategies())
-//                                         .create());
+
+        AppConfig.NETWORK_HOST_WS = "http://192.168.13.33:5005/api";
+        AppConfig.NETWORK_HOST_SR = "http://192.168.13.33:5005/";
+        AppConfig.NETWORK_HOST_WEB = "http://192.168.13.33:5005/";
+
+        AppConfig.NETWORK_Samim_HUB = "MyMessangerHub";
+        AppConfig.NETWORK_HOST_TRY_CONNECT_DURATION = 1000 * 3;
+        AppConfig.NETWORK_HOST_PING = AppConfig.NETWORK_HOST_SR + "TestConnection/Ping.html";
+         AppConfig.NETWORK_HOST_TRY_CONNECT_EMERGENCY_DURATION = 1000 * 10;//1000 * 60 * 30;
+
+        AppConfig.AUTO_REFRESH_INTERVAL = 30;//minute
+
+        AppConfig.setGsonSetting(new GsonBuilder()
+                                         .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                                         .excludeFieldsWithModifiers(Modifier.TRANSIENT)
+                                         .registerTypeAdapter(Date.class, new DateSerializer())
+                                         .registerTypeAdapter(ChatBaseContent.class, new ChatBaseContentDeserializer())
+                                         .registerTypeAdapter(ChatContentType.class, new ChatContentTypeDeserializer())
+                                         .setExclusionStrategies(new ExclusionGsonStrategies())
+                                         .create());
         //======================================================================================================
     }
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         SamimAction.ACTIVATION_EXPIRED = "ir.hfj.samim.parent.activation.expired";
         SamimAction.ACTIVATION_SUCCESSFUL = "ir.hfj.samim.parent.activation.successful";
         SamimAction.ACTIVATION_NULL = "ir.hfj.samim.parent.activation.null";
@@ -63,7 +93,8 @@ public class AppSamim extends App {
     }
 
     @Override
-    protected void ActiveAndroidInitialize() {
+    protected void ActiveAndroidInitialize()
+    {
         Configuration.Builder config = new Configuration.Builder(this);
         //
         config.setDatabaseName("messanger.db");
@@ -75,18 +106,18 @@ public class AppSamim extends App {
                 StudentModel.class,
                 TeacherModel.class
         );
-        //
-//        config.addTypeSerializers(
-//                ChatContentDateSerializer.class,
-//                ChatContentTypeDateSerializer.class,
-//                UUIDSerializer.class,
-//                BigDecimalSerializer.class,
-//                CalendarSerializer.class,
-//                SqlDateSerializer.class,
-//                UtilDateSerializer.class,
-//                FileSerializer.class,
-//                TypeSerializer.class);
-        //
+
+        config.addTypeSerializers(
+                ChatContentDateSerializer.class,
+                ChatContentTypeDateSerializer.class,
+                UUIDSerializer.class,
+                BigDecimalSerializer.class,
+                CalendarSerializer.class,
+                SqlDateSerializer.class,
+                UtilDateSerializer.class,
+                FileSerializer.class,
+                TypeSerializer.class);
+
         ActiveAndroid.initialize(config.create());
         TeacherModel t1 = new TeacherModel(UUID.fromString("9a2f9673-ca44-4bbd-af03-4d75fa4388a1"));
         t1.name = "سجاد عبدالملکی";
@@ -97,30 +128,40 @@ public class AppSamim extends App {
 
     }
 
-    public static AppSamim getInstance(Activity activity) {
+    public static AppSamim getInstance(Activity activity)
+    {
         return ((AppSamim) activity.getApplication());
     }
 
     @Override
-    public void onTerminate() {
+    public void onTerminate()
+    {
         super.onTerminate();
         unregisterReceiver(mActivationReceiver);
     }
 
-    BroadcastReceiver mActivationReceiver = new BroadcastReceiver() {
+    BroadcastReceiver mActivationReceiver = new BroadcastReceiver()
+    {
         @Override
-        public void onReceive(Context context, Intent intent) {
-            if (AppConfig.DEBUG) {
+        public void onReceive(Context context, Intent intent)
+        {
+            if (AppConfig.DEBUG)
+            {
                 Log.i(AppConfig.LOG_TAG, "App Take BroadcastReceiver: " + intent.getAction());
             }
-            if (intent.getAction().equalsIgnoreCase(SamimAction.ACTIVATION_SUCCESSFUL)) {
+            if (intent.getAction().equalsIgnoreCase(SamimAction.ACTIVATION_SUCCESSFUL))
+            {
                 mUserSetting = DbBase.UserSetting.select();
 
-            } else if (intent.getAction().equalsIgnoreCase(SamimAction.ACTIVATION_EXPIRED)) {
-                if (mUserSetting == null) {
+            }
+            else if (intent.getAction().equalsIgnoreCase(SamimAction.ACTIVATION_EXPIRED))
+            {
+                if (mUserSetting == null)
+                {
                     mUserSetting = DbBase.UserSetting.select();
                 }
-                if (mUserSetting != null) {
+                if (mUserSetting != null)
+                {
                     mUserSetting.expired = true;
                 }
 
